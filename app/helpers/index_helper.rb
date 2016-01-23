989d00c1b1cae3cafb277bfa694e1c7da33020ec
Maria Alejandra <mariaalejandra@softwarecriollo.com>
2016-01-23 17:42:17 -0430

    Adding devise and creating home: styles

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -3,4 +3,17 @@ module IndexHelper
   def format_date(date)
     date.strftime("%Y-%m-%d")
   end
+
+  def resource_name
+  :user
+  end
+
+  def resource
+  @resource ||= User.new
+  end
+
+  def devise_mapping
+  @devise_mapping ||= Devise.mappings[:user]
+  end
+    
 end
