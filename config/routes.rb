c814e14f899baa58841d7d7a3658301cc99ea388
Maria Alejandra <mariaalejandra@softwarecriollo.com>
2016-01-23 03:12:22 -0430

    Adding Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'index#home'
 
   resources :opinions, only: [:new, :create]
